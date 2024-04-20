b56b2e9eefacaa482adbdd9f7f21ed16a6353165
QuocBao <quocbaovu15101996@gmail.com>
2024-04-20 17:47:48 +0700

    fix: animated scroll after re-open

--- src/index.tsx
+++ src/index.tsx
@@ -225,8 +225,8 @@ const ModalizeBase = (
     alwaysOpenValue: number | undefined,
     dest: TOpen = 'default',
   ): void => {
+    dragY.setValue(0);
     const { timing, spring } = openAnimationConfig;
-
     (backButtonListenerRef as any).current = BackHandler.addEventListener(
       'hardwareBackPress',
       handleBackPress,
